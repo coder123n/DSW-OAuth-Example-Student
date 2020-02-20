4de3248c712971e6ea2d7a7edde3826ceddc1bdf
coder123n <54326477+coder123n@users.noreply.github.com>
2020-02-20 09:03:05 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -78,7 +78,7 @@ def renderPage1():
 
 @app.route('/page2')
 def renderPage2():
-     if 'user_data' in session:
+    if 'user_data' in session:
         user_data_pprint = pprint.pformat(session['user_data'])#format the user data nicely
     else:
         user_data_pprint = '';
